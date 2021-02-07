a18cca7f80a10dfa59c5da105972d0f6871feef8
AndryRana <lsourcus@gmail.com>
2021-02-07 01:34:07 +0100

    Using Native Device Features (Camera,Maps, Location - 290. PlaceList & Place Provider Setup  - 291. Adding the *Add Place* Screen & An Image Input - 292. Using Image Picker & The Device Camera- 293. Storing the Image on the Filesystem (on the Device) - 294. Managing Data & Images via the Provider Package - 297. Preparing SQLite

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+import '../models/place.dart';
+import '../helpers/db_helper.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  void addPlace(String pickedTitle, File pickedImage) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: pickedTitle,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+    DBHElper.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+}
